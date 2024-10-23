e04bc3411b3337c3d031bd0aff478eaf2d303725
Mahesh Kumar <mahkumar@chegg.com>
2024-10-23 19:02:25 +0530

    async functions

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 import asyncio
-import re
 import pyaudio
 import torch 
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
@@ -16,12 +15,11 @@ app = FastAPI()
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
-
 TRANSCRIPTION_MODEL_NAME = "openai/whisper-large-v3-turbo"
 
 # Audio settings
-STEP_IN_SEC: int = 1    # We'll increase the processable audio data by this
-LENGTH_IN_SEC: int = 7    # We'll process this amount of audio data together maximum
+STEP_IN_SEC: int = 1
+LENGTH_IN_SEC: int = 7
 NB_CHANNELS = 1
 RATE = 16000
 CHUNK = RATE
@@ -30,13 +28,10 @@ CHUNK = RATE
 WHISPER_LANGUAGE = "en"
 WHISPER_THREADS = 1
 
-# Visualization (expected max number of characters for LENGHT_IN_SEC audio)
+# Visualization
 MAX_SENTENCE_CHARACTERS = 128
 
-audio_buffer = deque(maxlen=LENGTH_IN_SEC)
-
-
-device_name = torch.device("cuda") if torch.cuda.is_available() else ("mps" if torch.mps.is_available() else "cpu")
+device_name = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
 device = torch.device(device_name)
 torch_dtype = torch.bfloat16
 
@@ -52,15 +47,16 @@ transcription_pipeline = pipeline(
     model=transcription_model,
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
-    chunk_length_s = min(LENGTH_IN_SEC, 30), # it uses sliding window and other protocol
+    chunk_length_s = min(LENGTH_IN_SEC, 30),
     torch_dtype=torch_dtype,
     device=device,
 )
 
 logger.info(f"{TRANSCRIPTION_MODEL_NAME} loaded")
 
-audio_buffer = deque(maxlen=LENGTH_IN_SEC * CHUNK)
-START = False
+global audio_buffer, START, RESUMING
+audio_buffer = asyncio.Queue(maxsize=LENGTH_IN_SEC * CHUNK)
+START = asyncio.Event()
 RESUMING = False
 active_connections = set()
 
@@ -89,8 +85,7 @@ async def send_transcription(transcription: str):
     for connection in active_connections:
         await connection.send_text(transcription)
 
-def producer_thread():
-    global START
+async def producer_task():
     audio = pyaudio.PyAudio()
     stream = audio.open(
         format=pyaudio.paInt16,
@@ -100,77 +95,76 @@ def producer_thread():
         frames_per_buffer=CHUNK,
     )
 
-    while START:
+    while START.is_set():
         try:
-            audio_data = stream.read(CHUNK, exception_on_overflow=False)
-            audio_buffer.append(audio_data)
-            logger.info(f"Producer added data. Buffer size: {len(audio_buffer)}")
+            audio_data = await asyncio.to_thread(stream.read, CHUNK, exception_on_overflow=False)
+            await audio_buffer.put(audio_data)
+            logger.info(f"Producer added data. Buffer size: {audio_buffer.qsize()}")
         except Exception as e:
-            logger.error(f"Error in producer thread: {str(e)}")
+            logger.error(f"Error in producer task: {str(e)}")
 
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
-async def consumer_thread():
-    global START
-    while START:
-        try:
-            if len(audio_buffer) >= LENGTH_IN_SEC:
-                audio_data_to_process = b''.join(list(audio_buffer))
-                audio_buffer.clear()
+async def transcribe(audio_data_array):
+    return await asyncio.to_thread(
+        transcription_pipeline,
+        {"array": audio_data_array, "sampling_rate": RATE},
+        return_timestamps=True,
+        generate_kwargs={"language": "english", "return_timestamps": True, "max_new_tokens": MAX_SENTENCE_CHARACTERS}
+    )
 
+async def consumer_task():
+    while START.is_set():
+        try:
+            if audio_buffer.qsize() >= LENGTH_IN_SEC:
+                audio_data_to_process = b''.join([await audio_buffer.get() for _ in range(LENGTH_IN_SEC)])
                 audio_data_array = np.frombuffer(audio_data_to_process, np.int16).astype(np.float32) / 32768.0
 
-                transcription = transcription_pipeline(
-                    {"array": audio_data_array, "sampling_rate": RATE},
-                    return_timestamps=True,
-                    generate_kwargs={"language": "english", "return_timestamps": True, "max_new_tokens": MAX_SENTENCE_CHARACTERS}
-                )["text"]
+                transcription = await transcribe(audio_data_array)
+                transcription_text = transcription["text"].rstrip(".")
 
-                transcription = transcription.rstrip(".")
-
-                if transcription:
-                    await send_transcription(transcription)
-                    logger.info(f"Sent transcription: {transcription}")
+                if transcription_text:
+                    await send_transcription(transcription_text)
+                    logger.info(f"Sent transcription: {transcription_text}")
             else:
                 await asyncio.sleep(0.1)
         except Exception as e:
-            logger.error(f"Error in consumer thread: {str(e)}")
+            logger.error(f"Error in consumer task: {str(e)}")
 
-        logger.info(f"Consumer thread iteration - Buffer size: {len(audio_buffer)}")
-
+        logger.info(f"Consumer task iteration - Buffer size: {audio_buffer.qsize()}")
 
 @app.post("/start")
 async def start_transcription():
-    global START, RESUMING, audio_buffer
-    if not START:
-        START = True
+    global audio_buffer, RESUMING
+    if not START.is_set():
+        START.set()
         RESUMING = False
-        audio_buffer.clear()  # Clear the buffer when starting a new session
-        threading.Thread(target=producer_thread).start()
-        asyncio.create_task(consumer_thread())
+        # Clear the buffer by creating a new Queue
+        audio_buffer = asyncio.Queue(maxsize=LENGTH_IN_SEC * CHUNK)
+        asyncio.create_task(producer_task())
+        asyncio.create_task(consumer_task())
     return {"status": "started"}
 
 @app.post("/resume")
 async def resume_transcription():
-    global START, RESUMING
-    if not START:
-        START = True
+    if not START.is_set():
+        START.set()
+        global RESUMING
         RESUMING = True
-        threading.Thread(target=producer_thread).start()
-        asyncio.create_task(consumer_thread())
+        asyncio.create_task(producer_task())
+        asyncio.create_task(consumer_task())
     return {"status": "resumed"}
 
 @app.post("/stop")
 async def stop_transcription():
-    global START
-    START = False
+    START.clear()
     return {"status": "stopped"}
 
 async def status_check():
     while True:
-        logger.info(f"Thread status - START: {START}, Buffer size: {len(audio_buffer)}")
+        logger.info(f"Task status - START: {START.is_set()}, Buffer size: {audio_buffer.qsize()}")
         await asyncio.sleep(5)  # Check every 5 seconds
 
 @app.on_event("startup")
