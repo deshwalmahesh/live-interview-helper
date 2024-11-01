cf07073b94ea6e189237ba4231e8078b4e4431e2
Mahesh Kumar <mahkumar@chegg.com>
2024-11-01 13:35:36 +0700

    Added diarization, no of speaker input

---
+++ fastapi_app/helpers.py
@@ -0,0 +1,102 @@
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from pyannote.audio import Pipeline as DiarizationPipeline
+import asyncio, re, torch
+
+def remove_blacklisted_words(string:str, ignore_case:bool=False) -> str:
+    """
+    Given a string and a replacement map, it returns the replaced string.
+    Fastest Way: Gistfrom best answer: https://stackoverflow.com/questions/6116978/how-to-replace-multiple-substrings-of-a-string
+    args:
+        string: string to execute replacements on
+        replacements: replacement dictionary {value to find: value to replace}
+        ignore_case: whether the match should be case insensitive
+    return: Processed string
+    """
+    REPLACEMENTS = {"Okay": "", "Thank you": "", "Hmm": ""}
+    
+    if not REPLACEMENTS: # Edge case that'd produce a funny regex and cause a KeyError
+        return string
+    
+    # If case insensitive, we need to normalize the old string so that later a replacement
+    # can be found. For instance with {"HEY": "Greetings!"} we should match and find a replacement for "hey", "HEY", "hEy", etc.
+    if ignore_case:
+        def normalize_old(s): return s.lower()
+
+        re_mode = re.IGNORECASE
+
+    else:
+        def normalize_old(s): return s
+        re_mode = 0
+
+    REPLACEMENTS = {normalize_old(key): val for key, val in REPLACEMENTS.items()}
+    
+    # Place longer ones first to keep shorter substrings from matching where the longer ones should take place
+    # For instance given the replacements {'ab': 'AB', 'abc': 'ABC'} against the string 'hey abc', it should produce
+    # 'hey ABC' and not 'hey ABc'
+    rep_sorted = sorted(REPLACEMENTS, key=len, reverse=True)
+    rep_escaped = map(re.escape, rep_sorted)
+    
+    # Create a big OR regex that matches any of the substrings to replace
+    pattern = re.compile("|".join(rep_escaped), re_mode)
+    
+    # For each match, look up the new string in the replacements, being the key the normalized old string
+    return pattern.sub(lambda match: REPLACEMENTS[normalize_old(match.group(0))], string)
+
+
+
+# Whisper settings
+WHISPER_LANGUAGE = "english"
+TRANSCRIPTION_MODEL_NAME = "openai/whisper-large-v3-turbo"
+MAX_SENTENCE_CHARACTERS = 128
+
+# Diarization settings
+NUM_SPEAKERS = 2
+
+device_name = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
+device = torch.device(device_name)
+torch_dtype = torch.bfloat16
+
+# ------ Transcription Helpers ------
+transcription_model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    TRANSCRIPTION_MODEL_NAME, torch_dtype=torch_dtype, low_cpu_mem_usage=True
+)
+transcription_model.to(device)
+
+processor = AutoProcessor.from_pretrained(TRANSCRIPTION_MODEL_NAME)
+
+transcription_pipeline = pipeline(
+    "automatic-speech-recognition",
+    model=transcription_model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    chunk_length_s = 30, #  min(LENGTH_IN_SEC, 30)
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+
+async def transcribe(audio_data_array, RATE):
+    return await asyncio.to_thread(
+        transcription_pipeline,
+        {"array": audio_data_array, "sampling_rate": RATE},
+        return_timestamps=True,
+        generate_kwargs={"language": "english", "return_timestamps": True, "max_new_tokens": MAX_SENTENCE_CHARACTERS}
+    )
+
+
+# ---- Speaker Diarization ----
+
+# NOTE: If you lodd it first time, you need to have Accept all clauses foe the model and add a variable
+# use_auth_token="HUGGINGFACE_ACCESS_TOKEN_GOES_HERE")
+diarization_pipeline = DiarizationPipeline.from_pretrained(
+  "pyannote/speaker-diarization-3.1")
+
+
+diarization_pipeline.to(device)
+
+def diarize(audio_numpy_array, num_speakers, RATE):
+    diarization_result = diarization_pipeline({
+        "waveform": torch.from_numpy(audio_numpy_array).unsqueeze(0),  # Can use .to(device) but pipeline handles that
+        "sample_rate": RATE, "num_speakers": num_speakers})
+    return diarization_result
+
--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,71 +1,44 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
-import uvicorn
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import pyaudio
-import torch 
-from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 import numpy as np
-import threading
-from collections import deque
 import logging
 from pydantic import BaseModel
+from helpers import transcribe, diarize, TRANSCRIPTION_MODEL_NAME, remove_blacklisted_words
 
-app = FastAPI()
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
-TRANSCRIPTION_MODEL_NAME = "openai/whisper-large-v3-turbo"
+logger.info(f"{TRANSCRIPTION_MODEL_NAME} loaded")
+
+app = FastAPI()
+
+thread_pool = ThreadPoolExecutor(max_workers=1)
 
 # Audio settings
 STEP_IN_SEC: int = 1
-LENGTH_IN_SEC: int = 7
+LENGTH_IN_SEC: int = 3
 NB_CHANNELS = 1
 RATE = 16000
-CHUNK = RATE
-CHUNK_SIZE = RATE * LENGTH_IN_SEC
+CHUNK_SIZE = RATE * LENGTH_IN_SEC # Just a function dependent on the LENGTH IN SEC
 
-# Whisper settings
-WHISPER_LANGUAGE = "en"
-WHISPER_THREADS = 1
-
-# Visualization
-MAX_SENTENCE_CHARACTERS = 128
-
-device_name = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu")
-device = torch.device(device_name)
-torch_dtype = torch.bfloat16
-
-transcription_model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    TRANSCRIPTION_MODEL_NAME, torch_dtype=torch_dtype, low_cpu_mem_usage=True
-)
-transcription_model.to(device)
-
-processor = AutoProcessor.from_pretrained(TRANSCRIPTION_MODEL_NAME)
-
-transcription_pipeline = pipeline(
-    "automatic-speech-recognition",
-    model=transcription_model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    chunk_length_s = min(LENGTH_IN_SEC, 30),
-    torch_dtype=torch_dtype,
-    device=device,
-)
-
-logger.info(f"{TRANSCRIPTION_MODEL_NAME} loaded")
 
 global audio_buffer, START, RESUMING
-audio_buffer = asyncio.Queue(maxsize=LENGTH_IN_SEC * CHUNK)
+audio_buffer = asyncio.Queue(maxsize= CHUNK_SIZE)
 START = asyncio.Event()
 RESUMING = False
 active_connections = set()
 
-client_audio_buffer = asyncio.Queue(maxsize=LENGTH_IN_SEC * CHUNK)
+client_audio_buffer = asyncio.Queue(maxsize =CHUNK_SIZE)
 
 
 class TranscriptionRequest(BaseModel):
     transcription: list
+
+class TranscriptionConfig(BaseModel):
+    numSpeakers: int
 
 @app.post("/get_answers")
 async def get_answers(request: TranscriptionRequest):
@@ -95,12 +68,12 @@ async def producer_task():
         channels=NB_CHANNELS,
         rate=RATE,
         input=True,
-        frames_per_buffer=CHUNK,
+        frames_per_buffer=RATE,
     )
 
     while START.is_set():
         try:
-            audio_data = await asyncio.to_thread(stream.read, CHUNK, exception_on_overflow=False)
+            audio_data = await asyncio.to_thread(stream.read, RATE, exception_on_overflow=False)
             await audio_buffer.put(audio_data)
             logger.info(f"Producer added data. Buffer size: {audio_buffer.qsize()}")
         except Exception as e:
@@ -110,13 +83,6 @@ async def producer_task():
     stream.close()
     audio.terminate()
 
-async def transcribe(audio_data_array):
-    return await asyncio.to_thread(
-        transcription_pipeline,
-        {"array": audio_data_array, "sampling_rate": RATE},
-        return_timestamps=True,
-        generate_kwargs={"language": "english", "return_timestamps": True, "max_new_tokens": MAX_SENTENCE_CHARACTERS}
-    )
 
 async def consumer_task():
     while START.is_set():
@@ -125,12 +91,24 @@ async def consumer_task():
                 audio_data_to_process = b''.join([await audio_buffer.get() for _ in range(LENGTH_IN_SEC)])
                 audio_data_array = np.frombuffer(audio_data_to_process, np.int16).astype(np.float32) / 32768.0
 
-                transcription = await transcribe(audio_data_array)
-                transcription_text = transcription["text"].rstrip(".")
+                transcription_task = asyncio.create_task(transcribe(audio_data_array, RATE = RATE))
+
+                # Run diarization in a separate thread as it's giving error in async mode
+                diarization_task = asyncio.create_task(
+                    asyncio.to_thread(diarize, audio_data_array, NUM_SPEAKERS, RATE))
+
+                # Wait for both tasks to complete
+                transcription, diarization = await asyncio.gather(transcription_task, diarization_task)
+                logger.info(f"DIARIZATION:\n{diarization}")
+                
+
+                transcription_text = remove_blacklisted_words(transcription["text"]).rstrip(". ?!")
 
                 if transcription_text:
                     await send_transcription(transcription_text)
                     logger.info(f"Sent transcription: {transcription_text}")
+                
+
             else:
                 await asyncio.sleep(0.1)
         except Exception as e:
@@ -138,23 +116,25 @@ async def consumer_task():
 
         logger.info(f"Consumer task iteration - Buffer size: {audio_buffer.qsize()}")
 
+
 @app.post("/start")
-async def start_transcription():
-    global audio_buffer, RESUMING
+async def start_transcription(config: TranscriptionConfig):
+    global audio_buffer, RESUMING, NUM_SPEAKERS
+    NUM_SPEAKERS = config.numSpeakers
     if not START.is_set():
         START.set()
         RESUMING = False
-        # Clear the buffer by creating a new Queue
-        audio_buffer = asyncio.Queue(maxsize=LENGTH_IN_SEC * CHUNK)
+        audio_buffer = asyncio.Queue(maxsize=CHUNK_SIZE)
         asyncio.create_task(producer_task())
         asyncio.create_task(consumer_task())
     return {"status": "started"}
 
 @app.post("/resume")
-async def resume_transcription():
+async def resume_transcription(config: TranscriptionConfig):
+    global RESUMING, NUM_SPEAKERS
+    NUM_SPEAKERS = config.numSpeakers
     if not START.is_set():
         START.set()
-        global RESUMING
         RESUMING = True
         asyncio.create_task(producer_task())
         asyncio.create_task(consumer_task())
@@ -212,7 +192,7 @@ async def start_client_transcription():
         START.set()
         RESUMING = False
         # Clear the buffer by creating a new Queue
-        client_audio_buffer = asyncio.Queue(maxsize=LENGTH_IN_SEC * CHUNK)
+        client_audio_buffer = asyncio.Queue(maxsize = CHUNK_SIZE)
         asyncio.create_task(client_consumer_task())
     return {"status": "started"}
 
